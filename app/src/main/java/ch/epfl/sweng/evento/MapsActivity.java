236376b5ed95b3e51fd57b8aa38d5459c21bb355
MANQUAT Gautier <gautier.manquat@epfl.ch>
2015-10-14 17:22:40 +0200

    Test of adding the google api location to add a marker next to the location of the user

--- app/src/main/java/ch/epfl/sweng/evento/MapsActivity.java
+++ app/src/main/java/ch/epfl/sweng/evento/MapsActivity.java
@@ -1,8 +1,15 @@
 package ch.epfl.sweng.evento;
 
+import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,9 +17,23 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.Random;
+
+import static com.google.android.gms.common.api.GoogleApiClient.*;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        ConnectionCallbacks, OnConnectionFailedListener
+{
+    // LogCat tag
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     private GoogleMap mMap;
+    private Location mLastLocation;
+
+    // Google client to interact with Google API
+    private GoogleApiClient mGoogleApiClient;
+
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +44,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        // First we need to check availability of play services
+        if (checkPlayServices())
+        {
+            // Building the GoogleApi client
+            buildGoogleApiClient();
+        }
+    }
 
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        if (mGoogleApiClient != null)
+        {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        checkPlayServices();
     }
 
 
@@ -42,9 +85,88 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         mMap.setMyLocationEnabled(true);
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mLastLocation = LocationServices.FusedLocationApi
+                .getLastLocation(mGoogleApiClient);
+
+        if(mLastLocation != null)
+        {
+            double latitude = mLastLocation.getLatitude();
+            double longitude = mLastLocation.getLatitude();
+
+            Random random = new Random();
+
+            LatLng marker = new LatLng(latitude + random.nextDouble(),
+                    longitude + random.nextDouble());
+
+            mMap.addMarker(new MarkerOptions().position(marker).title("Marker in somewhere"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(marker));
+        }
+        else
+        {
+            // Add a marker in Sydney and move the camera
+            LatLng sydney = new LatLng(-34, 151);
+            mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        }
+    }
+
+    /**
+     * Create the Google API client object
+     */
+    protected synchronized void buildGoogleApiClient()
+    {
+        mGoogleApiClient = new Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    /**
+     * Method that verify the digestibility of the google play services
+     */
+    protected boolean checkPlayServices()
+    {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS)
+        {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode))
+            {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            }
+            else
+            {
+                Toast.makeText(getApplicationContext(), "This device is not supported",
+                        Toast.LENGTH_LONG).show();
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    //----------------------------------------------------------------------------------------------
+
+    /**
+     * Google API callback
+     */
+    @Override
+    public void onConnected(Bundle bundle)
+    {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i)
+    {
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult)
+    {
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = "
+                + connectionResult.getErrorCode());
     }
 }
