e25ce9c470a4454b80220e252addbb7caf7536c8
MANQUAT Gautier <gautier.manquat@epfl.ch>
2015-10-22 01:08:11 +0200

    it's working for the maps part, but not completely: no more google+ API => no location => no marker no main screen as a mosaic but should be quickly fix tomorrow

--- Client/Evento/app/src/main/java/ch/epfl/sweng/evento/tabsFragment/MapsFragment.java
+++ Client/Evento/app/src/main/java/ch/epfl/sweng/evento/tabsFragment/MapsFragment.java
@@ -3,146 +3,29 @@ package ch.epfl.sweng.evento.tabsFragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 
 import ch.epfl.sweng.evento.R;
 
-import static com.google.android.gms.common.api.GoogleApiClient.*;
 
 /**
  * Created by Gautier on 21/10/2015.
  */
-public class MapsFragment extends Fragment implements
-        OnMapReadyCallback,
-        ConnectionCallbacks,
-        OnConnectionFailedListener
+public class MapsFragment extends Fragment
 {
-    // LogCat tag
-    private static final String TAG = MapsFragment.class.getSimpleName();
-
-    private static View mView;
-
-    private static GoogleMap mMap;
-
-    // Google client to interact with Google API
-    private GoogleApiClient mGoogleApiClient;
-
-    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 1000;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getFragmentManager()
-                .findFragmentById(R.id.map2);
-        mapFragment.getMapAsync(this);
-
-        // First we need to check availability of play services
-        if (checkPlayServices())
-        {
-            // Building the GoogleApi client
-            buildGoogleApiClient();
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState)
     {
-        if (container == null)
-        {
-            return null;
-        }
+        View view = inflater.inflate(R.layout.fragment_maps, container, false);
 
-        mView = inflater.inflate(R.layout.fragment_maps, container, false);
+        CustomMapFragment mapFragment = new CustomMapFragment();
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.add(R.id.map2, mapFragment).commit();
 
-        return mView;
-    }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap)
-    {
-        mMap = googleMap;
-
-        mMap.setMyLocationEnabled(true);
-    }
-
-    /**
-     * Create the Google API client object
-     */
-    protected synchronized void buildGoogleApiClient()
-    {
-        mGoogleApiClient = new Builder(getContext())
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .build();
-    }
-
-    /**
-     * Method that verify the availability of the google play services
-     */
-    protected boolean checkPlayServices()
-    {
-        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getContext());
-        if (resultCode != ConnectionResult.SUCCESS)
-        {
-            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode))
-            {
-                GooglePlayServicesUtil.getErrorDialog(resultCode, getActivity(),
-                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
-            }
-            else
-            {
-                Toast.makeText(getContext(), "This device is not supported",
-                        Toast.LENGTH_LONG).show();
-                try {
-                    throw new Exception();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public void onConnected(Bundle bundle) {
-
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult)
-    {
-        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = "
-                + connectionResult.getErrorCode());
+        return view;
     }
 }
