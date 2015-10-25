0f29a4b9fdfdd20b562c4ff5bb30d576d96518d0
ESLIER Thomas <thomas.eslier@epfl.ch>
2015-10-25 14:11:50 +0100

    Rename pager_item into fragment_mosaic

--- Client/Evento/app/src/main/java/ch/epfl/sweng/evento/tabsFragment/ContentFragment.java
+++ Client/Evento/app/src/main/java/ch/epfl/sweng/evento/tabsFragment/ContentFragment.java
@@ -50,7 +50,7 @@ public class ContentFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.pager_item, container, false);
+        return inflater.inflate(R.layout.fragment_mosaic, container, false);
     }
 
     @Override
