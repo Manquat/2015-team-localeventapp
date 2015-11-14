e5b721d5aa796a45bc1a883582caf8c4de0fd072
MANQUAT Gautier <gothlego@hotmail.com>
2015-11-14 16:25:50 +0100

    trying to resolve the problem with the EventActivity.java

--- Client/Evento/app/src/main/java/ch/epfl/sweng/evento/EventActivity.java
+++ Client/Evento/app/src/main/java/ch/epfl/sweng/evento/EventActivity.java
@@ -28,17 +28,6 @@ public class EventActivity extends AppCompatActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         movementDetector = new GestureDetectorCompat(this, this);
 
