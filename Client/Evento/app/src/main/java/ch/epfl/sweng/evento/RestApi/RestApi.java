9d27394356eb5d4d00838fb49aaf62d62b2015cc
jmuth <joachim.h.muth@gmail.com>
2015-11-16 14:29:06 +0100

    correct server config according to new URL

--- Client/Evento/app/src/main/java/ch/epfl/sweng/evento/RestApi/RestApi.java
+++ Client/Evento/app/src/main/java/ch/epfl/sweng/evento/RestApi/RestApi.java
@@ -28,7 +28,7 @@ public class RestApi{
     private NetworkProvider mNetworkProvider;
     private String mUrlServer;
     // TODO: as soon as the server provide a better way to get event, change it
-    private int mNoEvent = 1;
+    private int mNoEvent = 5;
 
 
     public RestApi(NetworkProvider networkProvider, String urlServer){
