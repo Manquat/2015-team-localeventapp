580f655a66f7d3866c221030235b27f76d1cc493
MANQUAT Gautier <gothlego@hotmail.com>
2015-11-23 14:14:03 +0100

    correction to make the code compile

--- Client/Evento/app/src/main/java/ch/epfl/sweng/evento/tabsFragment/MapsFragment.java
+++ Client/Evento/app/src/main/java/ch/epfl/sweng/evento/tabsFragment/MapsFragment.java
@@ -23,8 +23,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.List;
 
 
 import ch.epfl.sweng.evento.EventDatabase;
