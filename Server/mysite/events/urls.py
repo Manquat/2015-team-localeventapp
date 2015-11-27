f01678fa34e5720fefc6dfa0bf118261f7e4ab9b
Christoph Schaefer <christophernstrerne.schaefer@epfl.ch>
2015-11-27 23:48:17 +0100

    Implemented Userclass, added manytomany participants fields changed creator to creatorid, added function put and delete participant and function geteventofcreator and get event of participant.

--- Server/mysite/events/urls.py
+++ Server/mysite/events/urls.py
@@ -9,9 +9,8 @@ from . import views
 urlpatterns = [
     url(r'^events/$', views.event_list),
     url(r'^events/(?P<pk>[0-9]+)$', views.event_detail),
-    url(r'^events/(?P<username>[\w|\W]+)$', views.event_detailuser),
+    url(r'^events/(?P<pk>[0-9]+)/(?P<pk2>[0-9]+)$', views.event_addparticipant),
     url(r'^events/(?P<fromdate>\d*)/(?P<todate>\d*)$', views.event_requestdate),
-    #Eventrequest gives back event information in function of date and distance, optional arguments are Number of Events (default = 100) and starting ID (default = 0)
     url(r'^events/(?P<fromdate>\d*)/(?P<todate>\d*)/(?P<mLongitude>[-+]?([0-9]*\.[0-9]+|[0-9]+))/(?P<mLatitude>[-+]?([0-9]*\.[0-9]+|[0-9]+))/(?P<mDistance>([0-9]*\.[0-9]+|[0-9]+))$', views.event_request),
     url(r'^events/(?P<fromdate>\d*)/(?P<todate>\d*)/(?P<mLongitude>[-+]?([0-9]*\.[0-9]+|[0-9]+))/(?P<mLatitude>[-+]?([0-9]*\.[0-9]+|[0-9]+))/(?P<mDistance>([0-9]*\.[0-9]+|[0-9]+))/(?P<mNE>\d*)$', views.event_request),
     url(r'^events/(?P<fromdate>\d*)/(?P<todate>\d*)/(?P<mLongitude>[-+]?([0-9]*\.[0-9]+|[0-9]+))/(?P<mLatitude>[-+]?([0-9]*\.[0-9]+|[0-9]+))/(?P<mDistance>([0-9]*\.[0-9]+|[0-9]+))/(?P<mNE>\d*)/(?P<mId>\d*)$', views.event_request),
