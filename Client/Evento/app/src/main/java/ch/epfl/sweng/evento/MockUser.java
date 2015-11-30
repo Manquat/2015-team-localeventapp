16d5ee7cd12f3db3b6ebf24eb3c5d5747294c068
jmuth <joachim.h.muth@gmail.com>
2015-11-30 14:13:15 +0100

    add a mock class MockUser, temporary. Will be replace by User when ready

---
+++ Client/Evento/app/src/main/java/ch/epfl/sweng/evento/MockUser.java
@@ -0,0 +1,16 @@
+package ch.epfl.sweng.evento;
+
+/**
+ * Created by joachimmuth on 30.11.15.
+ */
+public class MockUser {
+
+    public int getId(){
+        return 0;
+    }
+
+    public String getName(){
+        return "MockJo";
+    }
+
+}
