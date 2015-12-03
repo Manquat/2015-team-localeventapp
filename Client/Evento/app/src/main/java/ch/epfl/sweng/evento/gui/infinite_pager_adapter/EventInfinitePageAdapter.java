19fa39d2a8595724d20dd281251d947669f42db4
MANQUAT Gautier <gautier.manquat@epfl.ch>
2015-12-03 17:34:20 +0100

    comments working properly with mock user and no restAPI

--- Client/Evento/app/src/main/java/ch/epfl/sweng/evento/gui/infinite_pager_adapter/EventInfinitePageAdapter.java
+++ Client/Evento/app/src/main/java/ch/epfl/sweng/evento/gui/infinite_pager_adapter/EventInfinitePageAdapter.java
@@ -1,25 +1,24 @@
 package ch.epfl.sweng.evento.gui.infinite_pager_adapter;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Point;
-import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import ch.epfl.sweng.evento.Comment;
 import ch.epfl.sweng.evento.Conversation;
 import ch.epfl.sweng.evento.MockUser;
-import ch.epfl.sweng.evento.gui.ConversationActivity;
 import ch.epfl.sweng.evento.EventDatabase;
 import ch.epfl.sweng.evento.R;
 import ch.epfl.sweng.evento.event.Event;
@@ -28,13 +27,19 @@ import ch.epfl.sweng.evento.gui.ConversationAdapter;
 /**
  * An infinite page adapter for the event activity
  */
-public class EventInfinitePageAdapter extends InfinitePagerAdapter<Integer> {
+public class EventInfinitePageAdapter extends InfinitePagerAdapter<Integer> implements View.OnClickListener {
     Activity mActivity;
+    private Map<Integer, ListView> mListViews;
+    private Map<Integer, Boolean> mCurrentlyAddingAComment;
+    private Map<Integer, EditText> mMessagesBox;
 
     public EventInfinitePageAdapter(Integer initialEventSignature, Activity activity) {
         super(initialEventSignature);
 
         mActivity = activity;
+        mListViews = new HashMap<>();
+        mCurrentlyAddingAComment = new HashMap<>();
+        mMessagesBox = new HashMap<>();
     }
 
     @Override
@@ -66,26 +71,24 @@ public class EventInfinitePageAdapter extends InfinitePagerAdapter<Integer> {
         }
 
         ConversationAdapter conversationAdapter = new ConversationAdapter(mActivity, conversation);
-        ListView listView = (ListView) rootLayout.findViewById(R.id.event_list_comment);
-        listView.setAdapter(conversationAdapter);
+        ListView listOfComment = (ListView) rootLayout.findViewById(R.id.event_list_comment);
+        listOfComment.setAdapter(conversationAdapter);
 
         GridLayout layout = (GridLayout) inflater.inflate(R.layout.fragment_event,
-                listView, false);
+                listOfComment, false);
 
         updateFields(layout, event);
 
-        listView.addHeaderView(layout);
+        listOfComment.addHeaderView(layout);
 
         Button addCommentButton = new Button(mActivity);
         addCommentButton.setText(mActivity.getResources().getString(R.string.conversation_add_comment));
-        addCommentButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(mActivity, "test add comment", Toast.LENGTH_LONG).show();
-            }
-        });
+        mCurrentlyAddingAComment.put(currentEventId, false);
+        addCommentButton.setOnClickListener(this);
 
-        listView.addFooterView(addCommentButton);
+        listOfComment.addFooterView(addCommentButton);
+
+        mListViews.put(currentEventId, listOfComment);
 
         return rootLayout;
     }
@@ -116,5 +119,49 @@ public class EventInfinitePageAdapter extends InfinitePagerAdapter<Integer> {
                 mActivity.finish();
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        int currentEventId = getCurrentIndicator();
+
+        ListView listView = mListViews.get(currentEventId);
+        listView.removeFooterView(v);
+
+        if (mCurrentlyAddingAComment.get(currentEventId)) {
+            EditText messageBox = mMessagesBox.get(currentEventId);
+            String message = messageBox.getText().toString();
+
+            // creating the new Comment
+            //TODO use the restApi
+            EventDatabase.INSTANCE.getEvent(currentEventId).getConversation()
+                    .addComment(new Comment(new MockUser(), message));
+
+            listView.removeFooterView(messageBox);
+        }
+
+        addingFooterOfTheListView(currentEventId);
+    }
+
+    private void addingFooterOfTheListView(int currentEventID) {
+        Button addCommentButton = new Button(mActivity);
+        boolean currentlyAddingAComment = mCurrentlyAddingAComment.get(currentEventID);
+
+        if (currentlyAddingAComment) {
+            addCommentButton.setText(mActivity.getResources().getString(R.string.conversation_add_comment));
+        } else {
+            EditText message = new EditText(mActivity);
+            message.setHint(mActivity.getResources().getString(R.string.comment_message_hint));
+            mListViews.get(currentEventID).addFooterView(message);
+
+            mMessagesBox.put(currentEventID, message);
+
+            addCommentButton.setText(mActivity.getResources().getString(R.string.event_validate));
+        }
+
+        mCurrentlyAddingAComment.put(currentEventID, !currentlyAddingAComment);
+        addCommentButton.setOnClickListener(this);
+
+        mListViews.get(currentEventID).addFooterView(addCommentButton);
     }
 }
