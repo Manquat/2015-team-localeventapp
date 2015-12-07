d31dbc6864b9ccaaac1d24634b79efc1d7dbe3e2
ESLIER Thomas <thomas.eslier@epfl.ch>
2015-12-07 20:27:42 +0100

    host can add a user using his name

--- Client/Evento/app/src/main/java/ch/epfl/sweng/evento/gui/ManageActivity.java
+++ Client/Evento/app/src/main/java/ch/epfl/sweng/evento/gui/ManageActivity.java
@@ -1,12 +1,18 @@
 package ch.epfl.sweng.evento.gui;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -14,6 +20,7 @@ import java.util.List;
 
 import ch.epfl.sweng.evento.R;
 import ch.epfl.sweng.evento.Settings;
+import ch.epfl.sweng.evento.User;
 import ch.epfl.sweng.evento.gui.event_activity.EventActivity;
 import ch.epfl.sweng.evento.list_view.ListEntryAdapter;
 
@@ -23,6 +30,9 @@ import ch.epfl.sweng.evento.event.Event;
 import ch.epfl.sweng.evento.list_view.ListEntryAdapter;
 import ch.epfl.sweng.evento.rest_api.RestApi;
 import ch.epfl.sweng.evento.rest_api.callback.GetEventListCallback;
+import ch.epfl.sweng.evento.rest_api.callback.GetUserCallback;
+import ch.epfl.sweng.evento.rest_api.callback.GetUserListCallback;
+import ch.epfl.sweng.evento.rest_api.callback.HttpResponseCodeCallback;
 import ch.epfl.sweng.evento.rest_api.network_provider.DefaultNetworkProvider;
 
 public class ManageActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
@@ -85,17 +95,72 @@ public class ManageActivity extends AppCompatActivity implements AdapterView.OnI
 
         ListEntryAdapter.Entry item = (ListEntryAdapter.Entry) mItems.get(position);
         Toast.makeText(this, "You clicked " + item.getTitle(), Toast.LENGTH_SHORT).show();
+        final int innerPosition = position;
+        final Intent intent = new Intent(mActivity, EventActivity.class);
 
-        Intent intent = new Intent(mActivity, EventActivity.class);
+        // Create custom dialog object
+        final Dialog dialog = new Dialog(mActivity);
+        // Include dialog.xml file
+        dialog.setContentView(R.layout.dialog_manage);
+        // Set dialog title
+        dialog.setTitle(item.getTitle());
 
-        if(position < mHostedEvent.size()+1) {
-            intent.putExtra(EventActivity.CURRENT_EVENT_KEY, mHostedEvent.get(position-1).getID());
-        }
-        if(position > mHostedEvent.size()+1) {
-            intent.putExtra(EventActivity.CURRENT_EVENT_KEY, mMatchedEvent.get(position-mHostedEvent.size()-2).getID());
-        }
+        // set values for custom dialog components - text, image and button
+        final TextView text = (TextView) dialog.findViewById(R.id.text_manage_dialog);
+        ImageView image = (ImageView) dialog.findViewById(R.id.image_dialog);
+        image.setImageResource(R.drawable.football);
 
-        mActivity.startActivity(intent);
+        dialog.show();
+
+        Button addUserButton = (Button) dialog.findViewById(R.id.add_user);
+        // if decline button is clicked, close the custom dialog
+        addUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mRestAPI.getUserByName(new GetUserCallback() {
+                    @Override
+                    public void onDataReceived(User user) {
+                        if (user != null) {
+                            int id = 0;
+                            if (innerPosition < mHostedEvent.size() + 1) {
+                                id = mHostedEvent.get(innerPosition - 1).getID();
+                                Log.d(TAG, Integer.toString(id));
+                                mRestAPI.addParticipant(id, user.getUserId(), new HttpResponseCodeCallback() {
+                                    @Override
+                                    public void onSuccess(String response) {
+                                        Log.d(TAG, "Response" + response);
+                                        dialog.dismiss();
+                                    }
+                                });
+                            } else {
+                                Toast.makeText(mActivity.getApplicationContext(), "You can't add a user if you are not the host !", Toast.LENGTH_LONG).show();
+                            }
+                        } else {
+                            Toast.makeText(mActivity.getApplicationContext(), "No user called " + text.getText().toString().replace(" ", "%20"), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }, text.getText().toString().replace(" ", "%20"));
+
+            }
+        });
+
+        Button getEventPageButton = (Button) dialog.findViewById(R.id.get_event_page);
+        // if decline button is clicked, close the custom dialog
+        getEventPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (innerPosition < mHostedEvent.size() + 1) {
+                    intent.putExtra(EventActivity.CURRENT_EVENT_KEY, mHostedEvent.get(innerPosition - 1).getID());
+                }
+                if (innerPosition > mHostedEvent.size() + 1) {
+                    intent.putExtra(EventActivity.CURRENT_EVENT_KEY, mMatchedEvent.get(innerPosition - mHostedEvent.size() - 2).getID());
+                }
+
+                mActivity.startActivity(intent);
+                mActivity.finish();
+            }
+        });
+
 
     }
 
--- Client/Evento/app/src/main/java/ch/epfl/sweng/evento/gui/infinite_pager_adapter/EventInfinitePageAdapter.java
+++ Client/Evento/app/src/main/java/ch/epfl/sweng/evento/gui/infinite_pager_adapter/EventInfinitePageAdapter.java
@@ -74,11 +74,9 @@ public class EventInfinitePageAdapter extends InfinitePagerAdapter<Integer> {
         LinearLayout rootLayout = (LinearLayout) inflater.inflate(R.layout.event_adapter,
                 (ViewGroup) mActivity.getWindow().getDecorView().getRootView(), false);
 
-        mConversationAdapter = new ConversationAdapter(mActivity, currentEventId);
+        ConversationAdapter conversationAdapter = new ConversationAdapter(mActivity, currentEventId);
         ListView listOfComment = (ListView) rootLayout.findViewById(R.id.event_list_comment);
-
-        listOfComment.setAdapter(mConversationAdapter);
-
+        listOfComment.setAdapter(conversationAdapter);
 
         GridLayout layout = (GridLayout) inflater.inflate(R.layout.fragment_event,
                 listOfComment, false);
@@ -87,7 +85,7 @@ public class EventInfinitePageAdapter extends InfinitePagerAdapter<Integer> {
 
         listOfComment.addHeaderView(layout);
 
-        AddingComment.initialize(mActivity, listOfComment, currentEventId, mConversationAdapter);
+        AddingComment.initialize(mActivity, listOfComment, currentEventId, conversationAdapter);
 
 
         return rootLayout;
@@ -126,45 +124,16 @@ public class EventInfinitePageAdapter extends InfinitePagerAdapter<Integer> {
         ImageView pictureView = (ImageView) rootView.findViewById(R.id.eventPictureView);
         pictureView.setImageBitmap(currentEvent.getPicture());
         mExpListView = (ExpandableListView) rootView.findViewById(R.id.list_participant_exp);
-        prepareListData();
+
         ExpendableList mListAdapter = new ExpendableList(mActivity.getApplicationContext(), mListDataHeader, mListDataChild);
 
         // setting list adapter
-        mExpListView.setAdapter(mListAdapter);
-        mExpListView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mConversationAdapter.refresh();
-            }
-        });
 
         // configure the joint and unjoin button
-        //Button joinEventButton = (Button) rootView.findViewById(R.id.joinEvent);
-        //Button unJoinEventButton = (Button) rootView.findViewById(R.id.remove_user_from_event);
+        Button joinEventButton = (Button) rootView.findViewById(R.id.joinEvent);
+        Button unJoinEventButton = (Button) rootView.findViewById(R.id.remove_user_from_event);
 
-        //JoinEvent.initialize(mActivity, currentEvent.getID(), joinEventButton, unJoinEventButton,
-             //   mExpListView);
-    }
-
-    private void prepareListData() {
-        mListDataHeader = new ArrayList<String>();
-        mListDataChild = new HashMap<String, List<String>>();
-
-        // Adding child data
-        mListDataHeader.add("Participant of the event");
-
-        // Adding child data
-        List<String> participant = new ArrayList<String>();
-        participant.add("dff");
-        participant.add("ddf");
-        participant.add("dfdf");
-        participant.add(" ");
-        participant.add(" ");
-        participant.add(" ");
-        participant.add(" ");
-        participant.add(" ");
-
-
-        mListDataChild.put(mListDataHeader.get(0), participant);
+        JoinEvent.initialize(mActivity, currentEvent.getID(), joinEventButton, unJoinEventButton,
+                mExpListView);
     }
 }
