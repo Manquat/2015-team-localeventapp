25032493c57e98d73948a52acb8717406f196def
MANQUAT Gautier <gautier.manquat@epfl.ch>
2015-12-07 19:05:38 +0100

    correcting the problem with the address to long

--- Client/Evento/app/src/main/java/ch/epfl/sweng/evento/gui/infinite_pager_adapter/EventInfinitePageAdapter.java
+++ Client/Evento/app/src/main/java/ch/epfl/sweng/evento/gui/infinite_pager_adapter/EventInfinitePageAdapter.java
@@ -1,6 +1,7 @@
 package ch.epfl.sweng.evento.gui.infinite_pager_adapter;
 
 import android.app.Activity;
+import android.graphics.Point;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
